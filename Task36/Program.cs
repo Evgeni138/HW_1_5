6f687675fee6887dfbf87913e3503424f73a71aa
Evgeni <666997@bk.ru>
2022-04-24 20:27:52 +0800

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Укажите количество чисел в массиве");
+int a = Convert.ToInt32(Console.ReadLine());
+void array(int[] arr)
+{
+    for (int i = 0; i < a; i++)
+    arr[i] = new Random().Next(100, 1000);
+}
+void Print(int[] arr)
+{
+    for (int i = 0; i < a; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+void sum(int[] arr)
+{
+    int result = 0;
+    for (int i = 1; i < a; i += 2)
+    {
+        result += arr[i];
+    }
+    Console.WriteLine("-> "+result);
+}
+
+int[] array2 = new int[a];
+array(array2);
+Print(array2);
+sum(array2);
